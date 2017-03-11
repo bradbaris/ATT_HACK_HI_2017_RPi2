25ad39095a15fc34dc9d386f325065c4b5406d40
Bradford Baris <bradbaris@gmail.com>
2017-03-11 08:53:34 -1000

    Integrating TheBusHea API (WIP)

--- index.py
+++ index.py
@@ -4,8 +4,9 @@
 import os
 import json
 import time
-import serial     
+import serial
 import requests
+from lxml import etree     
 from random import randint
 
 ser = serial.Serial(
@@ -17,20 +18,27 @@ ser = serial.Serial(
     timeout=1
 )
 
-name = "User"
+busapi = requests.get(os.getenv('BUSHEA')+str(randint(12,20)))
+tree = etree.parse(busapi)
+loc = tree.xpath('/arrival/headsign')
+direction = tree.xpath('/arrival/direction')
+
 while 1:
     x=ser.readline()
     if(x != ''):
         print(x)
         if "NAME=" in x:
             name = x[5:-2]
-        num = randint(1,4)
-        if(num == 1):
-            payload = {"channel": "#doorbell-test", "username": "RailFare", "text": name+" purchased rail fare at Halekauwila station at 5:00PM", "icon_emoji": ":monorail:"}
-        if(num == 2):
-            payload = {"channel": "#doorbell-test", "username": "TheBusFare", "text": name+" purchased bus fare at Ala Moana station at 12:30PM", "icon_emoji": ":oncoming_bus:"}
-        if(num == 3):
-            payload = {"channel": "#doorbell-test", "username": "Bikeshare", "text": name+" purchased a bikeshare ride at Diamond Head station at 8:00AM", "icon_emoji": ":bikeshare:"}
+        choice = randint(1,4)
+        hour = str(randint(1,12))
+        minute = str(randint(10,59))
+        time = hour+":"+minute+"PM"
+        if(choice == 1):
+            payload = {"channel": "#doorbell-test", "username": "RailFare", "text": name+" purchased rail fare at Halekauwila station at "+time, "icon_emoji": ":monorail:"}
+        if(choice == 2):
+            payload = {"channel": "#doorbell-test", "username": "TheBusFare", "text": name+" purchased bus fare at "+loc+" ("+direction+") at "+time, "icon_emoji": ":oncoming_bus:"}
+        if(choice == 3):
+            payload = {"channel": "#doorbell-test", "username": "Bikeshare", "text": name+" purchased a bikeshare ride at Diamond Head station at "+time, "icon_emoji": ":bikeshare:"}
         if "SEND" in x:
             headers = {'Content-Type':'application/json'}
             r = requests.post(os.getenv('ENDPOINT'), headers=headers, data=json.dumps(payload))
