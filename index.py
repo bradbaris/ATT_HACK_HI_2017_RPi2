48ea6faff46b0efa3d986a4f1147c30c50a36f07
Bradford Baris <brad@nomadspaceship.com>
2017-03-11 06:40:58 -1000

    Parsed out the serial data

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 # http://raspberrypi.stackexchange.com/q/51498
+import os
+import json
 import time
 import serial     
 import requests
-
-url = 'https://hooks.slack.com/services/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'
-payload = {"channel": "#general", "username": "webhookbot", "text": "This is posted to #general and comes from a bot named webhookbot.", "icon_emoji": ":ghost:"}
+from random import randint
 
 ser = serial.Serial(
     port='/dev/ttyACM0',
@@ -16,10 +16,21 @@ ser = serial.Serial(
     bytesize=serial.EIGHTBITS,
     timeout=1
 )
-counter=0
+
+name = "User"
 while 1:
     x=ser.readline()
     if(x != ''):
         print(x)
-        if "SEND" in x: 
-            r = requests.post(url, data=payload)
+        if "NAME=" in x:
+            name = x[5:-1]
+        num = randint(1,4)
+        if(num == 1):
+            payload = {"channel": "#doorbell-test", "username": "RailFare", "text": name+" purchased rail fare at Halekauwila station at 5:00PM", "icon_emoji": ":monorail:"}
+        if(num == 2):
+            payload = {"channel": "#doorbell-test", "username": "TheBusFare", "text": name+" purchased bus fare at Ala Moana station at 12:30PM", "icon_emoji": ":oncoming_bus:"}
+        if(num == 3):
+            payload = {"channel": "#doorbell-test", "username": "Bikeshare", "text": name+" purchased a bikeshare ride at Diamond Head station at 8:00AM", "icon_emoji": ":bikeshare:"}
+        if "SEND" in x:
+            headers = {'Content-Type':'application/json'}
+            r = requests.post(os.getenv('ENDPOINT'), headers=headers, data=json.dumps(payload))
