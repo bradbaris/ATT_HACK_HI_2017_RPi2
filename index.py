cce2545510e08d790ead40df4f528737400e3883
Bradford Baris <brad@nomadspaceship.com>
2017-03-11 15:20:03 -1000

    refactor control flow to trigger less

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ import os
 import json
 import time
 import serial
+import random
 import requests
 import xml.etree.ElementTree as ET   
 from random import randint
@@ -18,27 +19,31 @@ ser = serial.Serial(
     timeout=1
 )
 
-busapi = requests.get("http://api.thebus.org/arrivals/", params=[('key', os.getenv('BUSHEA')),('stop', str(randint(12,20)))])
-root = ET.fromstring(busapi.text)
-headsign = root[2][3].text
-direction = root[2][5].text
+busNum = [12,13,14,15,16,17,18,19,20,22,23,24,26,27,28,29,30,31,33,34,35,36,37,38,39,40,41,43,44,45,46,47,48,49,50]
 name = "User"
+
 while 1:
     x=ser.readline()
     if(x != ''):
         print(x)
         if "NAME=" in x:
             name = x[5:-2]
-        choice = randint(1,4)
-        hour = str(randint(1,12))
-        minute = str(randint(10,59))
-        time = hour+":"+minute+"PM"
-        if(choice == 1):
-            payload = {"channel": "#doorbell-test", "username": "RailFare", "text": name+" purchased rail fare at '"+headsign+"' station at "+time, "icon_emoji": ":monorail:"}
-        if(choice == 2):
-            payload = {"channel": "#doorbell-test", "username": "TheBusFare", "text": name+" purchased bus fare at '"+headsign+"' ("+direction+") at "+time, "icon_emoji": ":oncoming_bus:"}
-        if(choice == 3):
-            payload = {"channel": "#doorbell-test", "username": "Bikeshare", "text": name+" purchased a bikeshare ride at '"+headsign+"' at "+time, "icon_emoji": ":bikeshare:"}
+            randBus = str(random.choice(busNum))
+            busApi = requests.get("http://api.thebus.org/arrivals/", params=[('key', os.getenv('BUSHEA')),('stop', randBus)])
+            root = ET.fromstring(busApi.text)
+            headSign = root[randint(2,25)][3].text
+            direction = root[2][5].text
+            hour = str(randint(1,12))
+            minute = str(randint(10,59))
+            time = hour+":"+minute+"PM"
+
+            choice = randint(1,4)
+            if(choice == 1):
+                payload = {"channel": "#doorbell-test", "username": "RailFare", "text": name+" purchased rail fare at '"+headSign+"' station at "+time, "icon_emoji": ":monorail:"}
+            if(choice == 2):
+                payload = {"channel": "#doorbell-test", "username": "TheBusFare", "text": name+" purchased bus fare at '"+headSign+"' ("+direction+") at "+time, "icon_emoji": ":oncoming_bus:"}
+            if(choice == 3 or choice == 4):
+                payload = {"channel": "#doorbell-test", "username": "Bikeshare", "text": name+" purchased a bikeshare ride at '"+headSign+"' at "+time, "icon_emoji": ":bikeshare:"}
         if "SEND" in x:
             headers = {'Content-Type':'application/json'}
             r = requests.post(os.getenv('ENDPOINT'), headers=headers, data=json.dumps(payload))
